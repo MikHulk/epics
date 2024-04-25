39670c84b279dccec6d843ed9b49acea327f2505
MV <m.viey@wanadoo.fr>
2024-04-25 22:48:21 +0200

    test: epic fixture

--- epics/tests.py
+++ epics/tests.py
@@ -8,50 +8,46 @@ class EpicTestCase(TestCase):
 
     def setUp(self):
         self.start_time = timezone.now()
-
-    def test_epic_properties(self):
-        epic = Epic.objects.create(
+        self.epic = Epic.objects.create(
             title="An epic test.",
             description="This is a test.",
         )
-        self.assertEqual(epic.title, "An epic test.")
-        self.assertEqual(epic.description, "This is a test.")
+
+    def tearDown(self):
+        self.start_time = timezone.now()
+        self.epic.delete()
+
+    def test_epic_properties(self):
+        self.assertEqual(self.epic.title, "An epic test.")
+        self.assertEqual(self.epic.description, "This is a test.")
         self.assertTrue(
-            epic.pub_date > self.start_time and epic.pub_date < timezone.now()
+            self.epic.pub_date > self.start_time and self.epic.pub_date < timezone.now()
         )
         self.assertEqual(
-            str(epic),
-            f"Epic({epic.pk}): {epic.title}",
+            str(self.epic),
+            f"Epic({self.epic.pk}): {self.epic.title}",
         )
 
     def test_us_creation(self):
-        epic = Epic.objects.create(
-            title="An epic test.",
-            description="This is a test.",
-        )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="A first story",
                 description="This is a first story",
             ),
             bulk=False,
         )
-        self.assertEqual(epic.stories.count(), 1)
-        epic.stories.add(
+        self.assertEqual(self.epic.stories.count(), 1)
+        self.epic.stories.add(
             UserStory(
                 title="Another story",
                 description="This is a second story",
             ),
             bulk=False,
         )
-        self.assertEqual(epic.stories.count(), 2)
+        self.assertEqual(self.epic.stories.count(), 2)
 
     def test_epic_stats(self):
-        epic = Epic.objects.create(
-            title="An epic test.",
-            description="This is a test.",
-        )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="us1",
                 description="This is a on going story",
@@ -59,14 +55,14 @@ class EpicTestCase(TestCase):
             ),
             bulk=False,
         )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="us2",
                 description="This is a just created story",
             ),
             bulk=False,
         )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="us3",
                 description="This is a finished story",
@@ -74,7 +70,7 @@ class EpicTestCase(TestCase):
             ),
             bulk=False,
         )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="us4",
                 description="This is a canceled story",
@@ -82,7 +78,7 @@ class EpicTestCase(TestCase):
             ),
             bulk=False,
         )
-        epic.stories.add(
+        self.epic.stories.add(
             UserStory(
                 title="us5",
                 description="This is a suspended story",
@@ -90,22 +86,22 @@ class EpicTestCase(TestCase):
             ),
             bulk=False,
         )
-        self.assertEqual(epic.stories.count(), 5)
-        stats = epic.stats
+        self.assertEqual(self.epic.stories.count(), 5)
+        stats = self.epic.stats
         self.assertEqual(stats.total, 5)
         self.assertEqual(stats.in_progress, 1)
         self.assertEqual(stats.created, 1)
         self.assertEqual(stats.suspended, 1)
         self.assertEqual(stats.canceled, 1)
         self.assertEqual(stats.finished, 1)
-        us5 = epic.stories.get(title="us5")
+        us5 = self.epic.stories.get(title="us5")
         us5.status = StoryStatus.IN_PROGRESS
         us5.save()
-        us2 = epic.stories.get(title="us2")
+        us2 = self.epic.stories.get(title="us2")
         us2.status = StoryStatus.IN_PROGRESS
         us2.save()
-        self.assertEqual(epic.stories.count(), 5)
-        stats = epic.stats
+        self.assertEqual(self.epic.stories.count(), 5)
+        stats = self.epic.stats
         self.assertEqual(stats.total, 5)
         self.assertEqual(stats.in_progress, 3)
         self.assertEqual(stats.created, 0)
